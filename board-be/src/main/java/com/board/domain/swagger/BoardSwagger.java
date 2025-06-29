ea64566ecca464270ac389041342d40b62939c5c
suhyun <qkrtngus116@naver.com>
2025-06-30 07:52:57 +0900

    refactor: 게시글 상세 조회 swagger 명세

--- board-be/src/main/java/com/board/domain/swagger/BoardSwagger.java
+++ board-be/src/main/java/com/board/domain/swagger/BoardSwagger.java
@@ -2,6 +2,7 @@ package com.board.domain.swagger;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.board.domain.dto.BoardDto;
 
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Board API", description = "게시글 정보 관련 API")
@@ -20,11 +22,22 @@ public interface BoardSwagger {
 		content = @Content(
 			schema = @Schema(implementation = BoardDto.Response.class))
 	)
-	// Page<BoardDto.Response> getBoardsPaged(
-	// 	final LoadStrategyType type,
-	// 	final Optional<Long> cursorId,
-	// 	final Pageable pageable);
 	Page<BoardDto.Response> getBoardsPaged(
 		final BoardDto.Request request,
 		final Pageable pageable);
+
+	@Operation(summary = "게시글 상세 조회", description = "게시글 상세 조회합니다.")
+	@ApiResponses({
+		@ApiResponse(
+			responseCode = "200",
+			description = "조회 성공",
+			content = @Content(
+				schema = @Schema(implementation = BoardDto.Response.class))
+		),
+		@ApiResponse(
+			responseCode = "404",
+			description = "조회 실패"
+		)
+	})
+	BoardDto.Response getBoardById(final Long id);
 }
